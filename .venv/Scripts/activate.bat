199bc1005240eada6372a8cf6f01181e70fb1f4a
Jiri Kalmar <kalmar.jiri@gmail.com>
2022-11-30 21:18:06 +0100

    Change virtual enviroment files

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\documents\programming shit\PYTHON\4_ADVANCED\blog_with_users\.venv
+set VIRTUAL_ENV=D:\documents\programming shit\PYTHON\4_ADVANCED\blog_finished\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
